cf11b5c8c1965599595f626c8b2e9e81df9b259b
Benson <bensonby@bensonby-desktop.(none)>
2012-05-10 17:13:14 +0800

    sort the transaction list by transaction date and ID (both desc)

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,7 +2,7 @@ class TransactionsController < ApplicationController
   # GET /transactions
   # GET /transactions.json
   def index
-    @transactions = Transaction.all
+    @transactions = Transaction.find(:all, :order => "transaction_date DESC, id DESC")
 
     respond_to do |format|
       format.html # index.html.erb
