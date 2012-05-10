af7ebd0ed4da3b640bd8f7670be6f3bae7b2cdb4
Benson <bensonby@bensonby-desktop.(none)>
2012-05-11 01:08:54 +0800

    Show list of transactions for an account

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,6 +3,8 @@ class Account < ActiveRecord::Base
   attr_accessible :name, :parent_id, :opening_balance
   belongs_to :parent, :class_name => 'Account'
   has_many :children, :class_name => 'Account', :foreign_key => 'parent_id', :dependent => :destroy
+  has_many :transaction_entries
+  has_many :transactions, :through => :transaction_entries
 
   def default_values
     self.opening_balance ||= 0
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,6 @@
 class Transaction < ActiveRecord::Base
   has_many :transaction_entries, :dependent => :destroy, :order => "debit_amount DESC"
+  has_many :accounts, :through => :transaction_entries
   attr_accessible :transaction_date, :description, :transaction_entries_attributes
   accepts_nested_attributes_for :transaction_entries, :reject_if => lambda { |a| a[:debit_amount].blank? }, :allow_destroy => true
   validates_presence_of :transaction_date
