aa71297ff48f6a23c5e43279773f196653219470
Benson <bensonby@bensonby-desktop.(none)>
2012-05-12 02:36:31 +0800

    add the validator - transaction has a total debit amount of zero

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,4 +5,12 @@ class Transaction < ActiveRecord::Base
   accepts_nested_attributes_for :transaction_entries, :reject_if => lambda { |a| a[:debit_amount].blank? }, :allow_destroy => true
   validates_presence_of :transaction_date
   validates_presence_of :description
+  validate :zero_balance
+
+  def zero_balance
+    total_amount = self.transaction_entries.reduce(0) do |sum, t|
+      sum + t.debit_amount
+    end
+    errors.add(:base, "All Transaction Entries must add up to zero") unless total_amount == 0
+  end
 end
