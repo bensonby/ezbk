ff23b11f9ee29719aad03a9558642630a98d6a76
Benson <bensonby@bensonby-desktop.(none)>
2012-05-13 12:22:53 +0800

    add missing route modification - for autocomplete of quick create transaction

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Fin::Application.routes.draw do
   root :to => 'accounts#index'
   get 'transaction_entries/autocomplete_account_name'
+  get 'transactions/autocomplete_transaction_tostring'
+  post 'transactions/quick_create'
+  
   resources :accounts
   resources :transaction_entries
   resources :transactions do
